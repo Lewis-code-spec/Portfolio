25e83b2ab8724d053fb46f43c2392ee8919f8899
Lewy <lewis.2899@hotmail.com>
2025-09-04 08:36:12 +0100

    Portfolio

---
+++ MyPortfolio.Api/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Api.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Portfolio.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ContactController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public ContactController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(ContactRequest request)
+    {
+        var apiKey = _config["SendGridApiKey"];
+        var client = new SendGridClient(apiKey);
+
+        var from = new EmailAddress("lewisnewtonpaine@hotmail.com", "Lewis Paine Portfolio");
+        var to = new EmailAddress("lewisnewtonpaine@hotmail.com"); // Where to send messages
+        var subject = $"New Contact Message from {request.Name}";
+        var plainTextContent = $"Name: {request.Name}\nEmail: {request.Email}\n\n{request.Message}";
+        var htmlContent = $"<strong>Name:</strong> {request.Name}<br/><strong>Email:</strong> {request.Email}<br/><br/>{request.Message}";
+
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        var response = await client.SendEmailAsync(msg);
+
+        return response.IsSuccessStatusCode ? Ok() : StatusCode(500, "Email failed to send.");
+    }
+}
---
+++ Portfolio/Portfolio/Components/Pages/Home.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Portfolio.Models;
+
+namespace Portfolio.Components.Pages;
+
+public partial class Home : ComponentBase
+{
+    [Inject]
+    private HttpClient? Http { get; set; }
+
+    private ContactRequest contactRequest = new ContactRequest();
+
+    private bool? isSuccess;
+
+    private async Task HandleValidSubmit()
+    {
+        try
+        {
+            var response = await Http!.PostAsJsonAsync("http://localhost:5099/api/Contact", contactRequest);
+            isSuccess = response.IsSuccessStatusCode;
+            contactRequest = new();
+        }
+        catch
+        {
+            isSuccess = false;
+        }
+    }
+}
